52732ad7d4e7127503fd706f5038f03b7a2dd159
Kwun Yeung <kwun@creativeworks.com.hk>
2017-12-26 00:25:21 +0800

    added material-kit css

--- client/main.js
+++ client/main.js
@@ -2,3 +2,6 @@
 
 import '/imports/startup/client';
 import '/imports/startup/both';
+import '/imports/ui/stylesheets/bootstrap.min.css';
+import '/imports/ui/stylesheets/material-kit.css';
+import '/imports/ui/stylesheets/styles.css';
