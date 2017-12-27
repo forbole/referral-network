fbfb15468c871f0c84b1ae51635aed0053528912
Kwun Yeung <kwun@creativeworks.com.hk>
2017-12-27 19:03:35 +0800

    user accounts

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import { Accounts, STATES } from 'meteor/std:accounts-ui';
 import DefaultLayout from '/imports/ui/layouts/Default.jsx';
 import EmptyLayout from '/imports/ui/layouts/Empty.jsx';
+
 import Home from '/imports/ui/pages/Home.jsx';
 // import About from './pages/about.jsx';
 // import NotFound from './pages/not_found.jsx';
@@ -10,18 +12,22 @@ import Home from '/imports/ui/pages/Home.jsx';
 
 // App component - represents the whole app
 export default class App extends Component {
-  render() {
-    return (
-      <Router>
-        <DefaultLayout path="/" component={Home} />
-          {/*}<Route path="/about" component={About} />*/}
-        {/*
-        <Route component={EmptyLayout}>
-          <Route path="/sign-in" component={SignIn} />
-        </Route>
-        <Route path="*" component={NotFound}/>
-        */}
-      </Router>
-    );
-  }
+    render() {
+        return (
+            <Router>
+              <Switch>
+                <DefaultLayout exact={true} path="/" component={Home} />
+                <DefaultLayout path="/signin" component={() => <div className="container"><Accounts.ui.LoginForm /></div>} />
+                <DefaultLayout path="/signup" component={() => <div className="container"><Accounts.ui.LoginForm formState={STATES.SIGN_UP} /></div>} />
+              </Switch>
+                {/*}<Route path="/about" component={About} />*/}
+              {/*
+              <Route component={EmptyLayout}>
+                <Route path="/sign-in" component={SignIn} />
+              </Route>
+              <Route path="*" component={NotFound}/>
+              */}
+            </Router>
+        );
+    }
 }
