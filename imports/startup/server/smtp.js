4bae9da5c20c16ad5027cfdcf37565d6d2d4ef92
Kwun Yeung <kwun@creativeworks.com.hk>
2018-02-21 17:11:28 +0800

    fixed a typo in smtp.js

--- imports/startup/server/smtp.js
+++ imports/startup/server/smtp.js
@@ -1,5 +1,5 @@
 let smtpUser = Meteor.settings.mailgun.username;
 let smtpPassword = Meteor.settings.mailgun.password;
 let smtpServer = Meteor.settings.mailgun.server;
-let smtpPort = Meteor.settngs.mailgun.port
+let smtpPort = Meteor.settings.mailgun.port
 process.env.MAIL_URL = 'smtp://{smtpUser}:{smtpPassword}@{smtpServer}:{smtpPort}';
