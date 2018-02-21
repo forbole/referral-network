8ff91eab1ee2270a4c5f38ff7b4fcaca9d30e8d8
Kwun Yeung <kwun@creativeworks.com.hk>
2018-02-21 19:25:19 +0800

    updated header with login buttons

--- imports/ui/components/Header.jsx
+++ imports/ui/components/Header.jsx
@@ -1,26 +1,49 @@
 import React, { Component } from 'react';
 
-const Header = (...props) => {
-  console.log(props);
-  return (
-    <nav className="navbar navbar-primary navbar-fixed-top">
-        <div className="container">
-            <div className="navbar-header">
-              <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#navigation-example">
-                  <span className="sr-only">Toggle navigation</span>
-                  <span className="icon-bar"></span>
-                  <span className="icon-bar"></span>
-                  <span className="icon-bar"></span>
-              </button>
-              <a className="navbar-brand" href="/">Forbole</a>
-              {(props.user)?
-              <div>has user</div>
-              :<div><a href="/login">Sign In</a> <a href="/signup">Register</a></div>}
-            </div>
+class Header extends Component {
+  // console.log(props);
+  constructor(props){
+      super(props);
+      this.state = {
+        user: props.currentUser?props.currentUser:''
+      }
+  }
 
-        </div>
-      </nav>
-  );
+  render(){
+    return (
+      <nav className="navbar navbar-primary navbar-fixed-top">
+          <div className="container">
+              <div className="navbar-header">
+                <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#navigation">
+                    <span className="sr-only">Toggle navigation</span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                </button>
+                <a className="navbar-brand" href="/">Forbole</a>
+              </div>
+
+              <div className="collapse navbar-collapse">
+            		<ul className="nav navbar-nav navbar-right">
+                  {(this.state.user)?
+                  <li>
+                    <a href="#">Hello, {this.state.user}</a>
+                  </li>:
+                  <>
+                    <li>
+                      <a href="/login">Sign In</a>
+                    </li>
+                    <li className="button-container">
+                      <a href="/signup" className="btn btn-white btn-round">
+            							<i className="material-icons">shopping_cart</i> Register
+            					</a>
+                    </li></>}
+        		    </ul>
+              </div>
+            </div>
+        </nav>
+    );
+  }
 }
 
 export default Header;
--- imports/ui/layouts/Default.jsx
+++ imports/ui/layouts/Default.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link, Match, Redirect, Switch } from 'react-router-dom';
-import Header from '/imports/ui/components/Header.jsx';
+import Header from '/imports/ui/components/HeaderContainer.js';
 import Footer from '/imports/ui/components/Footer.jsx';
 // import Navbar from './../ui/navigation/navbar';
 const DefaultLayout = ({component: Component, ...rest}) => {
   return (
     <Route {...rest} render={matchProps => (
       <div className="default">
-        <Header user={rest.user} />
+        <HeaderContainer />
           <div className="main">
             <Component {...matchProps} />
           </div>
