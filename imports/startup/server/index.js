6810ac6260beb0dd31471b75e03d40aed313372a
Kwun Yeung <kwun@creativeworks.com.hk>
2018-03-06 16:34:35 +0800

    removed swpssr and added server-render packages.

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,6 +1,6 @@
 // Import server startup through a single index entry point
 
-// import './fixtures.js';
-// import './register-api.js';
+import './fixtures.js';
+import './register-api.js';
 import './smtp.js';
 import './accounts.js';
