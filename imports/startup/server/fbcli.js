044de22ef8d6f477206ab56e82d83c52fdc7facf
Kwun Yeung <kwun@creativeworks.com.hk>
2018-05-25 19:34:01 +0800

    added Exec package

--- imports/startup/server/fbcli.js
+++ imports/startup/server/fbcli.js
@@ -1,2 +1,14 @@
+// All fbcli methods here
+
 import { Meteor } from 'meteor/meteor';
 
+let fbPath = Meteor.settings.fbcli.path;
+
+Meteor.methods({
+    // createAccount: (name, password) => {
+
+    // },
+    // sendCoin: (toAddr, amount, name, password, seq) => {}
+
+    // Exec.run('ls', ['-al', '/*'], console.log(, stderrHandler);
+});
--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -4,6 +4,7 @@ import './fixtures.js';
 import './register-api.js';
 import './smtp.js';
 import './accounts.js';
+import './fbcli.js';
 
 // Add GTM
 if (process.env.ROOT_URL == 'https://mvp.forbole.com')
