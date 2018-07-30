0a78ba338aea07f807b23d9e2944ef99687657a1
Kwun Yeung <kwun@creativeworks.com.hk>
2018-07-30 18:26:22 +0800

    added loading svg and fixed connection list

--- imports/api/contributions/methods.js
+++ imports/api/contributions/methods.js
@@ -1,10 +1,11 @@
 import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 import { Contributions } from './contributions.js';
-import moment from 'moment';
+// import moment from 'moment';
 
 Meteor.methods({
   'contributions.insert'(type, propId, userId, score){
+    console.log("contributions insert");
     check(type, String);
     check(propId, String);
     check(userId, String);
