6bc33ee677bfca590756330673c294ea7250baf5
Sherry Ma <sherry.hyma@gmail.com>
2018-07-30 13:29:16 +0800

    implement lcd for send contribs and send coin

--- imports/startup/server/fbcli.js
+++ imports/startup/server/fbcli.js
@@ -18,58 +18,78 @@ Meteor.methods({
     'fbcli.checkStatus': function(){
         this.unblock();
         let future = new Future();
-        let command = fbPath + 'checkstatus';
-        exec(command, (error, stdout, stderr) => {
-            if (error) {
-                console.log(error);
-                throw new Meteor.Error(500, command + " failed");
-            }
-            let lines = stdout.toString().split('\n');
-            future.return(lines[1]);
-        });
+        let status = HTTP.get(fbHost + '/node_info');
+    
+        future.return(status.content[0]);
+
         return future.wait();
     },
-    'fbcli.sendCoin': function (toAddr, amount, name, password, seq) {
+    'fbcli.sendCoin': function (toAddr, amount, name, password, seq, accnum, gas) {
         check(toAddr, String);
-        check(amount, String);
+        check(amount, Number); 
         check(name, String);
         check(password, String);
-        check(seq,Number);
+        check(seq, Number);
+        check(accnum, Number);
+        check(gas, Number);
         // console.log(this);
         this.unblock();
         let future = new Future();
-        let command = 'ls -la';
-        exec(command, (error, stdout, stderr) => {
-            if (error) {
-                console.log(error);
-                throw new Meteor.Error(500, command + " failed");
+
+        let coindata = '{"chain_id": "' + chainId + '", "name": "' + name + '", "password": "' + password + '", "sequence": "' + seq + '", "account_number": "' + accnum + '", "gas": "' + gas + '", "amount": [{"amount": "' + amount + '", "denom": "steak"}]}'
+        let data = JSON.parse(coindata);
+        console.log(data);
+        let sending = HTTP.post(fbHost + '/accounts/' + toAddr + '/send', {
+            data: data,
+            headers: {
+                'Content-Type': 'application/json'
             }
-            future.return(stdout.toString());
         });
+        future.return(sending.toString());
         return future.wait();
     },
-    'fbcli.sendContrib': function (toAddr, type, key, content, name, time, seq){
+    'fbcli.sendContrib': function (toAddr, type, key, name, time, seq, accnum, gas, content, votetype = ''){
         check(toAddr, String);
         check(type, String);
         check(key, String);
-        check(content, String);
         check(name, String);
         check(time, String);
         check(seq, Number);
+        check(accnum, Number);
+        check(gas, Number);
+        check(accnum, Number);
+        check(content, String);  // set content later for vote
+        check(votetype, String);  // empty string for other contrib except vote
 
         this.unblock();
         password = '1234567890';
 
         let future = new Future();
-        let command = fbPath+'sendContrib '+toAddr+' '+content+' 0 '+key+' '+time+' '+type+' '+name+' '+password+' '+chainId+' 0'; 
-
-        exec(command, (error, stdout, stderr) => {
-            if (error) {
-                console.log(error);
-                throw new Meteor.Error(500, command + " failed");
+        console.log("vote: " + votetype)
+        if (type == 'vote') {
+            if (votetype === "upvote") {
+                content = "7570766f7465" // upvote
+            } else if (votetype === "downvote") {
+                content = "646f776e766f7465"; // downvote
+            } else {
+                console.log("vote2: " + votetype)
+                console.error("wrong vote type.")
+                return future.wait()
             }
-            future.return(stdout.toString());
+        } 
+        let postdata = '{"name": "'+ name + '", "password": "' + password + '", "chain_id": "' + chainId + '", "sequence": "' + seq + '", "account_number": "' + accnum + '", "gas": "' + gas + '", "content": "' + content + '", "key": "' + key + '", "time": "' + time + '", "votetype": "' + votetype + '"}';
+         // let temp = {"name": "sherry", "password": "1234567890", "chain_id": "test", "sequence": "4", "account_number": "0", "content": "12341234", "key": "abdc2222", "gas": "200000", "time": "2018-07-27T16:56:00Z"}
+        // console.log(postdata);
+        // console.log(temp)
+        let data = JSON.parse(postdata);
+        console.log(data);
+        let recom = HTTP.post(fbHost + '/contrib/' + toAddr + '/' + type, {
+            data: data,
+            headers: {
+                'Content-Type': 'application/json'
+            }
         });
+        future.return(recom.toString()); 
         return future.wait();
     },
     'fbcli.createAccount': function (username) {
@@ -98,7 +118,7 @@ Meteor.methods({
                     "password":password,
                     "seed": seed.content
                 };
-
+                
                 let response = HTTP.post(fbHost +'/keys', {
                     data: account,
                     headers: {
--- imports/ui/users/Wallet.jsx
+++ imports/ui/users/Wallet.jsx
@@ -7,39 +7,39 @@ class Wallet extends Component {
         super(props);
     }
 
-    sendCoin = () => {
-        Meteor.call('fbcli.sendCoin', 'aaaa', 'bbbb', 'cccc', 'dddd', 0, (error, result) => {
+    sendCoin = () => {  // example: send 1 steak here
+        Meteor.call('fbcli.sendCoin', 'cosmosaccaddr1qlcz62t2j78643g2w33zdlg78l3662ev9c63j0', 1, 'sherry', '1234567890', 3, 0, 200000, (error, result) => {
             console.log(result);
         });
-    }
-
+    } 
+ 
     createAccount = () => {
-        Meteor.call('fbcli.createAccount', this.refs.username.value, (error, result) => {
+        Meteor.call('fbcli.createAccount', this.refs.username.value, (error, result) => { 
             console.log(result);
         });
     }
 
-    sendContribInvite = () => {
-        Meteor.call('fbcli.sendContribInvite', 'aaaa', 'bbbb', 'cccc', 'dddd', 0, (error, result) => {
+    sendContribInvite = () => {  //example input
+        Meteor.call('fbcli.sendContrib', 'cosmosaccaddr1f3uspmas856c5583kf2m2sg2kdv2sa4urdk5td' , 'invite', 'adcbef11', 'sherry', '2018-07-30T12:57:00Z', 4, 0, 200000, 'acbd2341', (error, result) => {
             console.log(result);
         });
     }
 
-    sendContribReco = () => {
-        Meteor.call('fbcli.sendContribReco', 'aaaa', 'bbbb', 'cccc', 'dddd', 0, (error, result) => {
-            console.log(result);
+    sendContribReco = () => {  // example input
+        Meteor.call('fbcli.sendContrib', 'cosmosaccaddr1f3uspmas856c5583kf2m2sg2kdv2sa4urdk5td' , 'recommend', 'abcdef12', 'sherry', '2018-07-30T12:58:00Z', 5, 0, 200000, 'acbd2341', (error, result) => {
+            console.log(result); 
         });
+    } 
+  
+    sendContribVote = () => {  // example input, the last param is necessary (votetype)
+        Meteor.call('fbcli.sendContrib', 'cosmosaccaddr1f3uspmas856c5583kf2m2sg2kdv2sa4urdk5td', 'vote', 'abcdef13', 'sherry', '2018-07-30T12:59:00Z', 6, 0, 200000, '766f7465', 'upvote', (error, result) => {
+            console.log(result); 
+        }); 
     }
-
-    sendContribVote = () => {
-        Meteor.call('fbcli.sendContribVote', 'aaaa', 'bbbb', 'cccc', 'dddd', 0, (error, result) => {
-            console.log(result);
-        });
-    }
-
+  
     checkStatus = () => {
         Meteor.call('fbcli.checkStatus', (error, result) => {
-            console.log(JSON.parse(result));
+            console.log(JSON.parse(result)); 
         })
     }
 
@@ -53,6 +53,7 @@ class Wallet extends Component {
                 <button className="btn btn-primary" onClick={this.sendContribReco}>Send Recommendation</button>
                 <button className="btn btn-primary" onClick={this.sendContribVote}>Send Vote</button>
                 <button className="btn btn-primary" onClick={this.checkStatus}>Check Status</button>
+                <button className="btn btn-primary" onClick={this.sendContribInvite}>Send Invitation</button>
             </div>
         )
         
