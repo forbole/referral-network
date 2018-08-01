03743a0648704fd17380e98f28ba4d9685abfecf
Kwun Yeung <kwun@creativeworks.com.hk>
2018-08-01 16:03:03 +0800

    updated header main menu styles

--- imports/ui/components/Header.jsx
+++ imports/ui/components/Header.jsx
@@ -31,49 +31,44 @@ class Header extends Component {
       }
     return (
       <nav className={"navbar navbar-primary navbar-fixed-top"+this.state.headClass} color-on-scroll="350">
-          <div className="container">
-              <div className="navbar-header">
-                <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#navigation">
-                    <span className="sr-only">Toggle navigation</span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                </button>
+          <div className="container header">
+              <div className="navbar-header ">
                 <Link className="navbar-brand" to="/"><img className="logo" src="/img/forbole-logo-white.svg" />Forbole</Link>
+                
               </div>
-
-              <div className="collapse navbar-collapse">
-                <ul className="nav navbar-nav navbar-right">
-                    <li className="dropdown">
-                      <Link to="#" className="dropdown-toggle profile-link" data-toggle="dropdown">
-                        <div className="avatar img-raised" style={bg}/> {Meteor.user().profile.name}
-                        <b className="caret"></b>
-                      </Link>
-                      <ul className="dropdown-menu dropdown-with-icons">
-                        <li>
-                          <Link to={"/@"+Meteor.user().username}>
-                            <i className="material-icons">account_balance</i> My Profile
-                          </Link>
-                        </li>
-                        <li>
-                          <Link to="/connections">
-                            <i className="material-icons">device_hub</i> Connections
-                          </Link>
-                        </li>
-                        <li>
-                          <Link to="/settings">
-                            <i className="material-icons">beach_access</i> Settings
-                          </Link>
-                        </li>
-                        <li>
-                          <Link to="#" onClick={this.logout}>
-                            <i className="material-icons">clear</i> Logout
-                          </Link>
-                        </li>
-                      </ul>
-                    </li>
+          <div class="navbar-right">
+            <ul className="nav">
+              <li className="dropdown">
+                <Link to="#" className="dropdown-toggle profile-link" data-toggle="dropdown">
+                  <div className="avatar img-raised" style={bg} /> <span class="visible-md-inline visible-lg-inline name">{Meteor.user().profile.name}
+                  <b className="caret"></b></span>
+                </Link>
+                <ul className="dropdown-menu dropdown-with-icons main-menu">
+                  <li>
+                    <Link to={"/@" + Meteor.user().username}>
+                      <i className="material-icons">account_balance</i> My Profile
+                              </Link>
+                  </li>
+                  <li>
+                    <Link to="/connections">
+                      <i className="material-icons">device_hub</i> Connections
+                              </Link>
+                  </li>
+                  <li>
+                    <Link to="/settings">
+                      <i className="material-icons">beach_access</i> Settings
+                              </Link>
+                  </li>
+                  <li>
+                    <Link to="#" onClick={this.logout}>
+                      <i className="material-icons">clear</i> Logout
+                              </Link>
+                  </li>
                 </ul>
-              </div>
+              </li>
+            </ul>
+          </div>
+
             </div>
         </nav>
     );}
