802b68b6fc7bf492dad3e66b956f9e37c9c35bfd
Kwun Yeung <kwun@creativeworks.com.hk>
2018-08-01 18:39:50 +0800

    add one more blog exaple on profile page

--- imports/ui/users/BlogList.jsx
+++ imports/ui/users/BlogList.jsx
@@ -6,7 +6,7 @@ class BlogList extends Component {
     }
 
     render() {
-        return <div className="blog-list">
+        return <div className="blog-list mansory">
             <BlogCard 
                 cardImage="https://cdn-images-1.medium.com/max/1000/1*ojh3nGw8gjvvhRQKRIE8Fw.jpeg"
                 category="Blockchain"
@@ -18,6 +18,18 @@ class BlogList extends Component {
                 likes={523}
                 comments={78}
                 shares={36}
+            />
+            <BlogCard
+                cardImage="https://cdn-images-1.medium.com/max/1000/1*dwNDlVkHVM4QJmgkcVJOJw.jpeg"
+                category="Cosmos"
+                title="Why we choose Cosmos over Ethereum"
+                name="Kwun Yeung"
+                headline="Entrepreneur in Blockchain & Machine Learning"
+                avatar="/img/faces/kwun-profile.jpg"
+                date="Apr 2, 2018"
+                likes={276}
+                comments={27}
+                shares={42}
             />
         </div>
     }
