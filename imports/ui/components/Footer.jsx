abfb7614dfb7e87a967124815d18467caa5fbabf
Kwun Yeung <kwun@forbole.com>
2019-01-01 02:24:01 +0800

    add footer motto

--- imports/ui/components/Footer.jsx
+++ imports/ui/components/Footer.jsx
@@ -10,24 +10,25 @@ const Footer = () => {
     <footer className="footer">
         <div className="container">
             <nav className="pull-left">
-        <ul>
-          <li>
-            <a href="https://www.forbole.com" target="_blank">About Us</a>
-          </li>
-          <li>
-            <a href="https://www.forbole.com/#contact" target="_blank">Contact Us</a>
-          </li>
-          <li>
-            <a href="https://medium.com/forbole/" target="_blank">Blog</a>
-          </li>
-          <li>
-            <a href="https://www.forbole.com/privacy-policy/" target="_blank">Privacy Policy</a>
-          </li>
-        </ul>
+              <ul>
+                <li>
+                  <a href="https://www.forbole.com" target="_blank">About Us</a>
+                </li>
+                <li>
+                  <a href="https://www.forbole.com/#contact" target="_blank">Contact Us</a>
+                </li>
+                <li>
+                  <a href="https://medium.com/forbole/" target="_blank">Blog</a>
+                </li>
+                <li>
+                  <a href="https://www.forbole.com/privacy-policy/" target="_blank">Privacy Policy</a>
+                </li>
+              </ul>
             </nav>
             <div className="copyright pull-right">
                 &copy; {year}, made with <i className="fa fa-heart heart"></i> by Forbole Limited.
             </div>
+            <div className="motto">Don't trust. Verify.</div>
         </div>
     </footer>
   );
