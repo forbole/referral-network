2a37e17e7b6cf75e80dacef1692fb36141848d8f
Kwun Yeung <kwun@forbole.com>
2019-01-03 01:58:15 +0800

    add connection.all back

--- imports/api/connections/server/publications.js
+++ imports/api/connections/server/publications.js
@@ -2,9 +2,9 @@ import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 import { Connections } from '../connections.js';
 
-// Meteor.publish('connections.all', function () {
-//   return Connections.find();
-// });
+Meteor.publish('connections.all', function () {
+  return Connections.find();
+});
 
 Meteor.publish('connections.user', function (userId){
   return Connections.find({users: {$in: [userId]}});
